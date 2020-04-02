90771740e4466766641d2493efbc9306fe45b536
de@itstall.de <de@itstall.de>
2020-04-02 16:47:27 +0200

    Moved some functions, saved zoom level on map

---
+++ app/src/main/java/de/itstall/freifunkfranken/controller/MapsFragmentListener.java
@@ -0,0 +1,39 @@
+package de.itstall.freifunkfranken.controller;
+
+import android.content.Intent;
+import android.location.Location;
+import android.provider.Settings;
+
+import com.google.android.gms.maps.GoogleMap;
+
+import de.itstall.freifunkfranken.view.MapsFragment;
+
+public class MapsFragmentListener implements GoogleMap.OnCameraIdleListener {
+    private MapsFragment mapsFragment;
+
+    public MapsFragmentListener(MapsFragment mapsFragment) {
+        this.mapsFragment = mapsFragment;
+    }
+
+    public CustomLocationListenerInterface CustomLocationListenerInterface() {
+        return new CustomLocationListenerInterface() {
+            @Override
+            public void onLocationChanged(Location location) {
+                mapsFragment.changeLocation(location);
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mapsFragment.startActivity(intent);
+            }
+        };
+    }
+
+    @Override
+    public void onCameraIdle() {
+        mapsFragment.sharedPreferences.edit()
+                .putInt("mapZoom", (int) mapsFragment.mMap.getCameraPosition().zoom)
+                .apply();
+    }
+}
--- app/src/main/java/de/itstall/freifunkfranken/view/MapsFragment.java
+++ app/src/main/java/de/itstall/freifunkfranken/view/MapsFragment.java
@@ -4,14 +4,12 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,92 +38,61 @@ import java.util.Objects;
 import de.itstall.freifunkfranken.R;
 import de.itstall.freifunkfranken.controller.CustomLocationListener;
 import de.itstall.freifunkfranken.controller.CustomLocationListenerInterface;
+import de.itstall.freifunkfranken.controller.MapsFragmentListener;
 import de.itstall.freifunkfranken.controller.RequestAps;
 import de.itstall.freifunkfranken.model.AccessPoint;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = MapsFragment.class.getSimpleName();
-    private GoogleMap mMap;
+    public GoogleMap mMap;
+    public SharedPreferences sharedPreferences;
     private View rootView;
-    private SharedPreferences sharedPreferences;
+    private String locationProvider;
     private ProgressDialog progressDialog = null;
     private LocationManager locationManager;
     private CustomLocationListener customLocationListener;
-    String locationProvider;
+    private CustomLocationListenerInterface locationListener;
+    private MapsFragmentListener mapsFragmentListener;
 
-    private CustomLocationListenerInterface locationListener = new CustomLocationListenerInterface() {
-        @Override
-        public void onLocationChanged(Location location) {
-            if (mMap != null) {
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                        new LatLng(
-                                location.getLatitude(),
-                                location.getLongitude()),
-                        mMap.getCameraPosition().zoom)
-                );
-                sharedPreferences.edit()
-                        .putInt("mapZoom", (int) mMap.getCameraPosition().zoom).apply();
-            }
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(intent);
-        }
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mapsFragmentListener = new MapsFragmentListener(this);
+        locationListener = mapsFragmentListener.CustomLocationListenerInterface();
     }
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater,
-            ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.maps_fragment, container, false);
 
         progressDialog = new ProgressDialog(rootView.getContext());
         progressDialog.setMessage(getResources().getString(R.string.mapLoading));
         progressDialog.show();
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
-                .findFragmentById(R.id.mapFragment);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
+
         if (mapFragment != null) mapFragment.getMapAsync(this);
 
-        sharedPreferences = rootView.getContext()
-                .getSharedPreferences(getResources().getString(R.string.app_name), 0);
+        sharedPreferences = rootView.getContext().getSharedPreferences(getResources().getString(R.string.app_name), 0);
 
-        getLocation();
+        changeLocation(getLocation());
 
         return rootView;
     }
 
     private void showApsOnMap() {
-        List<AccessPoint> accessPointList = new RequestAps(
-                Objects.requireNonNull(
-                        this.getContext()))
-                .getSortedList(
-                        sharedPreferences.getBoolean(
-                                "MapOfflineRouter",
-                                false)
-                        , 0);
+        List<AccessPoint> accessPointList = new RequestAps(Objects.requireNonNull(this.getContext())).getSortedList(sharedPreferences.getBoolean("MapOfflineRouter", false), 0);
 
         for (int i = 0; i < accessPointList.size(); i++) {
-            LatLng ap = new LatLng(
-                    accessPointList.get(i).getLat(), accessPointList.get(i).getLon());
-            MarkerOptions markerOptions = new MarkerOptions()
-                    .position(ap)
-                    .title(accessPointList.get(i).getName());
-            if (accessPointList.get(i).isOnline())
-                markerOptions.icon(
-                        BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-            else
-                markerOptions.icon(
-                        BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            LatLng ap = new LatLng(accessPointList.get(i).getLat(), accessPointList.get(i).getLon());
+            MarkerOptions markerOptions = new MarkerOptions().position(ap).title(accessPointList.get(i).getName());
+
+            if (accessPointList.get(i).isOnline()) {
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+            } else {
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            }
             mMap.addMarker(markerOptions);
         }
     }
@@ -143,6 +110,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
         mMap.setMyLocationEnabled(true);
+        mMap.setOnCameraIdleListener(mapsFragmentListener);
 
         showApsOnMap();
 
@@ -161,7 +129,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         getLocation();
     }
 
-    private void getLocation() {
+    private Location getLocation() {
         customLocationListener = new CustomLocationListener(locationListener);
         locationManager = (LocationManager) rootView.getContext()
                 .getApplicationContext()
@@ -174,70 +142,59 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                 )
                 .withListener(new MultiplePermissionsListener() {
                                   @Override
-                                  public void onPermissionsChecked(
-                                          MultiplePermissionsReport report
-                                  ) {
+                                  public void onPermissionsChecked(MultiplePermissionsReport report) {
                                       if (report.areAllPermissionsGranted()) {
-                                          if (
-                                                  ActivityCompat.checkSelfPermission(
-                                                          rootView.getContext(),
-                                                          Manifest
-                                                                  .permission
-                                                                  .ACCESS_FINE_LOCATION
-                                                  ) == PackageManager.PERMISSION_GRANTED
-                                                          && ActivityCompat.checkSelfPermission(
-                                                          rootView.getContext(),
-                                                          Manifest
-                                                                  .permission
-                                                                  .ACCESS_COARSE_LOCATION
-                                                  ) == PackageManager.PERMISSION_GRANTED
-                                          ) {
+                                          if (ActivityCompat.checkSelfPermission(rootView.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                                                  ActivityCompat.checkSelfPermission(rootView.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
                                               locationProvider = getEnabledLocationProvider();
 
                                               assert locationProvider != null;
-                                              locationManager
-                                                      .requestLocationUpdates(
-                                                              locationProvider,
-                                                              3000,
-                                                              25,
-                                                              customLocationListener);
+                                              locationManager.requestLocationUpdates(
+                                                      locationProvider,
+                                                      3000,
+                                                      25,
+                                                      customLocationListener);
                                           }
                                       }
                                   }
 
                                   @Override
-                                  public void onPermissionRationaleShouldBeShown(
-                                          List<PermissionRequest> permissions,
-                                          PermissionToken token
-                                  ) {
+                                  public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
                                       token.continuePermissionRequest();
                                   }
                               }
                 )
                 .check();
+        return locationManager.getLastKnownLocation(locationProvider);
     }
 
     private String getEnabledLocationProvider() {
-        LocationManager locationManager = (LocationManager) rootView.getContext()
-                .getSystemService(Context.LOCATION_SERVICE);
+        LocationManager locationManager = (LocationManager) rootView.getContext().getSystemService(Context.LOCATION_SERVICE);
 
         //Kriterien um den LocationProvider zu finden
         Criteria criteria = new Criteria();
 
         //Gebe Namen des Providers zurück, der auf die Kriterien am besten passt
+        assert locationManager != null;
         String bestProvider = locationManager.getBestProvider(criteria, true);
 
+        assert bestProvider != null;
         boolean enabled = locationManager.isProviderEnabled(bestProvider);
 
         if (!enabled) {
-            Toast.makeText(
-                    rootView.getContext(),
-                    "No location provider enabled!",
-                    Toast.LENGTH_LONG
-            ).show();
+            Toast.makeText(rootView.getContext(), "No location provider enabled!", Toast.LENGTH_LONG).show();
 
             return null;
         }
         return bestProvider;
+    }
+
+    public void changeLocation(Location location) {
+        if (mMap != null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()),
+                    sharedPreferences.getInt("mapZoom", 10)));
+        }
     }
 }
